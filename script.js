a85690720eb632e4546a01d15f48aa141186560b
Andrea <onemanshow2099@gmail.com>
2022-03-18 08:33:54 +0100

    consegna principale finita

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ for (var i = 1; i < 101; i++) {
 
     
 
+    if((i % 3 == 0) && (i % 5 != 0)) {
+        document.getElementById("contenitore").innerHTML += `<div class="elemento"><p>fizz</p></div>`;
+    } else if((i % 5 == 0) && (i % 3 != 0)) {
+        document.getElementById("contenitore").innerHTML += `<div class="elemento"><p>buzz</p></div>`;
+    } else if ((i % 3 == 0) && (i % 5 == 0)) {
+        document.getElementById("contenitore").innerHTML += `<div class="elemento"><p>fizzBuzz</p></div>`;
+    } else {
+        document.getElementById("contenitore").innerHTML += `<div class="elemento"><p>${i}</p></div>`;
+    }
+
 }
+
+
