5c5f9749661746701fad1966546fbd3e782b0bab
Andrea <onemanshow2099@gmail.com>
2022-03-18 08:48:56 +0100

    finito i bonus

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ for (var i = 1; i < 101; i++) {
     
 
     if((i % 3 == 0) && (i % 5 != 0)) {
-        document.getElementById("contenitore").innerHTML += `<div class="elemento"><p>fizz</p></div>`;
+        document.getElementById("contenitore").innerHTML += `<div class="elemento fizz"><p>fizz</p></div>`;
     } else if((i % 5 == 0) && (i % 3 != 0)) {
-        document.getElementById("contenitore").innerHTML += `<div class="elemento"><p>buzz</p></div>`;
+        document.getElementById("contenitore").innerHTML += `<div class="elemento buzz"><p>buzz</p></div>`;
     } else if ((i % 3 == 0) && (i % 5 == 0)) {
-        document.getElementById("contenitore").innerHTML += `<div class="elemento"><p>fizzBuzz</p></div>`;
+        document.getElementById("contenitore").innerHTML += `<div class="elemento fizzbuzz"><p>fizzBuzz</p></div>`;
     } else {
-        document.getElementById("contenitore").innerHTML += `<div class="elemento"><p>${i}</p></div>`;
+        document.getElementById("contenitore").innerHTML += `<div class="elemento normale"><p>${i}</p></div>`;
     }
 
 }
